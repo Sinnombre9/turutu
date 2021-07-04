2a69b84c9fbf35a5d23e651fab6ff7d205c7db60
Sinnombre9 <83794209+Sinnombre9@users.noreply.github.com>
2021-07-04 12:39:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, box2, box3,box4;
+var box1, box2, box3,box4,box5,box6,box7;
 var hero,monster,rope,ground;
 
 function preload() {
@@ -24,9 +24,11 @@ function setup() {
 
   box1 = new Box(600, 100, 70, 70);
   box2 = new Box(900, 100, 70, 70);
-  box3 = new Box(900, 100, 70, 70);
-  box4 = new Box(900, 100, 70, 70);
-
+  box3 = new Box(900, 170, 70, 70);
+  box4 = new Box(900, 240, 70, 70);
+  box5 = new Box(900, 310, 70, 70);
+  box6 = new Box(900, 380, 70, 70);
+  box7 = new Box(900, 450, 70, 70);
 }
 
 function draw() {
@@ -37,9 +39,15 @@ function draw() {
   box2.display();
   box3.display();
   box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
 
   hero.display();
   rope.display();
   monster.display();
 
+}
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body,{x:mouseX,y:mouseY})
 }
